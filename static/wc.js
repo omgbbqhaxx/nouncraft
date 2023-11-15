8f8ec0b5b1943d4aa16a43c1792afdfab080ffd2
yasin aktimur <yasinaktimur@gmail.com>
2023-11-15 03:25:05 +0300

    blockies

--- static/wc.js
+++ static/wc.js
@@ -136,7 +136,24 @@ async function fetchAccountData() {
     clone.querySelector(".balance").textContent = humanFriendlyBalance;
 
     accountContainer.appendChild(clone);
+
+   // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+   var icon = blockies.create({ // All options are optional
+    seed: address, // seed used to generate icon data, default: random
+    //color: '#dfe', // to manually specify the icon color, default: random
+    //bgcolor: '#aaa', // choose a different background color, default: random
+    size: 15, // width/height of the icon in blocks, default: 8
+    scale: 3, // width/height of each block in pixels, default: 4
+    //spotcolor: '#000' // each pixel has a 13% chance of being of a third color,
+    // default: random. Set to -1 to disable it. These "spots" create structures
+    // that look like eyes, mouths and noses.
   });
+  console.log(icon);
+   $('.blockimage').attr('src', icon.toDataURL());
+   //$(".blockimage").append(icon);
+   // -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*// -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+  });
+ 
 
   await Promise.all(rowResolvers);
   document.querySelector("#prepare").style.display = "none";
